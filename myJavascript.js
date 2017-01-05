0388e6f0da9f119fa1f53276be498b2c7ac49a40
LucasMcL <LMcLaughlin09@gmail.com>
2017-01-05 11:55:58 -0600

    added selection options for gender and country

--- myJavascript.js
+++ myJavascript.js
@@ -1,8 +1,30 @@
 //sample data modified from randomUser.me
+var template = $('#myTemplate').html()
+var templateCompiled = Handlebars.compile(template)
+
+$(document).ready(() => {
+
+  // Disable placeholder options upon load
+  $('option[value="select"]').attr('disabled', true)
+
+  $('#findMatch').click(() => {
+    getUserData()
+      .then(function(val) {
+        randomUser = val
+
+        var output = templateCompiled(randomUser)
+        $('#match').html(output)
+      })
+  })
+})
+
 function getUserData() {
   return new Promise(function(resolve, reject) {
+    var gender = $('#seekingGender').val()
+    var country = $('#country').val()
+
     $.ajax({
-      url: 'https://randomuser.me/api/',
+      url: `https://randomuser.me/api/?nat=${country}&gender=${gender}`,
       dataType: 'json',
     })
     .done(function(data) {
@@ -10,56 +32,6 @@ function getUserData() {
     })
   })
 }
-
-getUserData()
-  .then(function(val) {
-    randomUser = val
-
-    console.dir(randomUser)
-
-    var template = $('#myTemplate').html()
-    var templateCompiled = Handlebars.compile(template)
-    var output = templateCompiled(randomUser)
-    $('#user2').append(output)
-
-
-  })
-
-var randomUser = {
-  "results": [
-    {
-      "gender": "female",
-      "name": {
-        "title": "ms",
-        "first": "stella",
-        "last": "meyer"
-      },
-      "location": {
-        "street": "7385 kapellenweg",
-        "city": "schmalkalden-meiningen",
-        "state": "saarland",
-        "postcode": 58225
-      },
-      "email": "stella.meyer@example.com",
-      "picture": {
-        "medium": "https:\/\/randomuser.me\/api\/portraits\/med\/women\/50.jpg"
-      },
-      "nat": "DE"
-    }
-  ]
-}
-
-//handlebars step one: grap the html from the script tag
-var template = $('#myTemplate').html()
-
-//handlebars step two: compile it into a template
-var templateCompiled = Handlebars.compile(template)
-
-//handlebars step three:render the HTML by passing the data to the template
-var output = templateCompiled(randomUser)
-
-//handlebars step four: put the complete HTML into the DOM
-$('#user').append(output)
 
 
 
