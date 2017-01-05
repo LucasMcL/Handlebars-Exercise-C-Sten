1872d8b6cdac95a181eee3135659f8d901b95c3d
LucasMcL <LMcLaughlin09@gmail.com>
2017-01-05 10:22:11 -0600

    assignment complete

--- myJavascript.js
+++ myJavascript.js
@@ -24,13 +24,25 @@ var randomUser = {
 }
 
 //handlebars step one: grap the html from the script tag
-
+var template = $('#myTemplate').html()
 
 //handlebars step two: compile it into a template
-
+var templateCompiled = Handlebars.compile(template)
 
 //handlebars step three:render the HTML by passing the data to the template
-
+var output = templateCompiled(randomUser)
 
 //handlebars step four: put the complete HTML into the DOM
+$('#user').append(output)
+
+
+
+
+
+
+
+
+
+
+
 
